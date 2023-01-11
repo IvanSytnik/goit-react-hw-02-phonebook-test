6d3235d7d79c69d35abedc813c543a6b085a8bd6
Ivan Sytnik <ivan.sytnik02@gmail.com>
2023-01-11 04:02:14 +0200

    not css and propTypes

---
+++ src/components/RenderContacts/RenderContacts.jsx
@@ -0,0 +1,21 @@
+import css from './RenderContacts.module.css'
+import PropTypes from 'prop-types';
+
+const RenderContacts =({items, contactsDelete}) => {
+  
+    return(
+    <div>
+    <h2>Contacts </h2>
+    <ul> 
+      {items.map(({id, name, number}) => (
+      <li
+       key={id}>{name}:{number}
+       <button type="button" onClick={() => contactsDelete(id)}>Delete</button>
+      </li>
+     ))}
+     </ul>
+   
+   </div> 
+   )}
+
+   export default RenderContacts;
